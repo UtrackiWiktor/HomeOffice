04aab87942059428c418d1322aae13088267a538
DominB <dominikkrzysztofik1@wp.pl>
2021-02-22 10:48:56 +0100

    Wyświetlanie tasków

--- HomeOffice/Views/ManagerView.xaml.cs
+++ HomeOffice/Views/ManagerView.xaml.cs
@@ -3,6 +3,10 @@ using System.Windows.Controls;
 using HomeOffice.classes.Users;
 using HomeOffice.classes.Tasks;
 using System.Collections.Generic;
+using HomeOffice.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
 
 namespace HomeOffice.Views
 {
@@ -12,12 +16,18 @@ namespace HomeOffice.Views
     public partial class ManagerView : UserControl
     {
         Manager manager;
-        Task t;
-        TaskDictionary task;
+        Task t = new Task();
+        TaskDictionary task = new TaskDictionary();
         public delegate void RefreshList();
         public event RefreshList RefreshListEvent;
-        public ManagerView()
+        public void SetUser(User u)
         {
+            manager = new Manager(u);
+        }
+      
+            public ManagerView()
+        {
+            manager = new Manager(((MainWindow)Application.Current.MainWindow).GetUser());
             InitializeComponent();
         }
 
@@ -40,17 +50,27 @@ namespace HomeOffice.Views
 
         private void tasksGrid_Loaded(object sender, RoutedEventArgs e)
         {
-           // tasksGrid.ItemsSource = t.AllTasksToList();
+            tasksGrid.ItemsSource = t.AllTasksToList();
         }
 
         private void empGrid_Loaded(object sender, RoutedEventArgs e)
         {
-          //  empGrid.ItemsSource = Query z pracowenikami z unitu managera
+            using (var DbContext = new HomeOfficeContext())
+            {
+                var query = DbContext.Users.Where(u => u.Unit == manager.Unit).ToList();
+                empGrid.ItemsSource = query;
+            }
+            
         }
 
         private void taskDicGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            //taskDicGrid.ItemsSource = task.AllTaskDictionaryToList();
+            taskDicGrid.ItemsSource = task.AllTaskDictionaryToList();
+           /* using (var DbContext = new HomeOfficeContext())
+            {
+                var query = DbContext.TaskDictionary.Where(t => t.Unit == manager.Unit).ToList();
+                taskDicGrid.ItemsSource = query;
+            }*/
         }
 
         private void filterButton2nd_Click(object sender, RoutedEventArgs e)
